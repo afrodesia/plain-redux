54cb7b427c9fbeb80fdb6933882b2a4b2b33452a
Ken Torry <kentorry@afrodesiamedia.com>
2017-04-06 04:22:20 -0500

    add RESET action

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -12,6 +12,10 @@ function counter(currentState, action){
 		case 'MINUS':
 			nextState.count = currentState.count - 1
 			return nextState
+			break;
+		case 'RESET':
+			nextState.count = 0
+			return nextState		
 		default:
 			console.log('In Default')
 			return currentState
@@ -39,6 +43,9 @@ document.getElementById('minus')
 	.addEventListener('click', function(){
 	store.dispatch({ type: 'MINUS'})
 })
-
+document.getElementById('reset')
+	.addEventListener('click', function(){
+	store.dispatch({ type: 'RESET'})
+})
 
 
