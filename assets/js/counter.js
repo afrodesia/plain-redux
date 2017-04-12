daed3de6c3d6c88277ba44891265ddd2f17d8cb6
Ken Torry <kentorry@afrodesiamedia.com>
2017-04-12 14:40:29 -0500

    Dashboard - New Panel inner content fix

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,6 +1,6 @@
 // Reducers
 
-function counter(state, action){
+function counterReducer(state, action){
 
 	if(typeof state === 'undefined'){
 		return { count: 0 }
@@ -26,10 +26,38 @@ function counter(state, action){
 	}
 }
 // STORE
+function todoReducer(state, action){
+	if(typeof state === 'undefined'){
+		return { todos: [] }
+	}
+	var nextState = Object.assign({},state)
+
+	
+	switch(action.type){
+		case 'NEW':
+			nextState.todos.push(action.payload)
+			return nextState
+			break;
+		case 'DELETE':
+			nextState.todos.pop()
+			return nextState
+			break;
+		case 'DELETEALL':
+			nextState.todos = []
+			return nextState		
+		default:
+			console.log('In Default')
+			return state
+	}
+
+}
+
+
+
 
 var state = { count: 0 }
 var store = Redux.createStore(counter)
-var counterEl = document.getElementById('counter')
+var counterEl = document.getElementById('counterReducer')
 console.log(store)
 
 function render(){
@@ -59,3 +87,15 @@ document.getElementById('reset')
 })
 
 
+document.getElementById('new')
+	.addEventListener('click', function(){
+	store.dispatch({ type: 'NEW', payload: })
+})
+document.getElementById('delete')
+	.addEventListener('click', function(){
+	store.dispatch({ type: 'DELETE'})
+})
+document.getElementById('delete-all')
+	.addEventListener('click', function(){
+	store.dispatch({ type: 'DELETEALL'})
+})
